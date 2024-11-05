b65d7cdaabbfaf17f7e2058732afcdc2f647adda
lucabunino <66553738+lucabunino@users.noreply.github.com>
2024-11-05 14:06:58 +0100

    Temporary

--- landing/src/routes/+layout.server.js
+++ landing/src/routes/+layout.server.js
@@ -4,13 +4,13 @@ export const load = async () => {
   const colorPairs = [
     ['#FFFBC1', '#BA5826', '/drawings/1.webp', '/drawings/1-mobile.webp',
      '/img/chair.webp', 'max-width: 45vw; max-height: 70vh; bottom: var(--gutter); right: 10vw',
-     '/img/chair-mobile.webp','max-width: 90vw; max-height: 40vh; top: 47%; left: 50%; transform: translate(-50%, -50%)'],
+     '/img/chair-mobile.webp','max-width: 90vw; max-height: calc(30vh + 20vw); top: 47%; left: 50%; transform: translate(-50%, -50%)'],
     ['#10069F', '#FFFBC1', '/drawings/2.webp', '/drawings/2-mobile.webp',
      '/img/table.webp', 'max-width: 45vw; max-height: 70vh; bottom: 5vh; right: 10vw',
-     '/img/table.webp','max-width: 75vw; max-height: 34vh; top: 44%; left: 50%; transform: translate(-50%, -50%)'],
+     '/img/table.webp','max-width: 75vw; max-height: calc(27vh + 20vw); top: 44%; left: 50%; transform: translate(-50%, -50%)'],
     ['#FFFBC1', '#10069F', '/drawings/3.webp', '/drawings/3-mobile.webp',
      '/img/armchair.webp', 'max-width: 50vw; max-height: 75vh; bottom: var(--gutter); right: 10vw',
-     '/img/armchair.webp', 'max-width: 90vw; max-height: 37vh; top: 45%; left: 50%; transform: translate(-50%, -50%)'],
+     '/img/armchair.webp', 'max-width: 90vw; max-height: calc(29vh + 20vw); top: 45%; left: 50%; transform: translate(-50%, -50%)'],
   ];
 
   // Randomly select a color pair
